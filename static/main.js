72b524c1b8989f5f56024e2cdbb277e66a01f794
rqguzman <rafael.guzman@outlook.com>
2018-05-20 12:25:50 -0300

    Updated the UI

--- static/main.js
+++ static/main.js
@@ -7,6 +7,10 @@
         .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
             function ($scope, $log, $http, $timeout) {
 
+                $scope.submitButtonText = 'submit';
+                $scope.loading = false;
+                $scope.urlerror = false;
+
                 $scope.getResults = function () {
 
                     $log.log("test");
@@ -21,10 +25,14 @@
                     success(function (results) {
                         $log.log(results);
                         getWordCount(results);
-
+                        $scope.wordcounts = null;
+                        $scope.loading = true;
+                        $scope.submitButtonText = 'Loading...';
+                        $scope.urlerror = false;
                     }).
                     error(function (error) {
                         $log.log(error);
+
                     });
 
                 };
@@ -41,6 +49,8 @@
                                 $log.log(data, status);
                             } else if (status === 200) {
                                 $log.log(data);
+                                $scope.loading = false;
+                                $scope.submitButtonText = 'submit';
                                 $scope.wordcounts = data;
                                 $timeout.cancel(timeout);
                                 return false;
@@ -48,6 +58,12 @@
                             // continue to call the poller() function every 2 seconds
                             // until the timeout is cancelled
                             timeout = $timeout(poller, 2000);
+                        }).
+                        error(function (error) {
+                            $log.log(error);
+                            $scope.loading = false;
+                            $scope.submitButtonText = 'Submit';
+                            $scope.urlerror = true;
                         });
                     };
                     poller();
